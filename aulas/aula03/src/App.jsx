596d0994913212f5d018f1eb9d60297444d2298e
MarcosAltec <marcospassos2211@gmail.com>
2024-08-27 21:26:49 -0300

    Subindo aula03 e pratica 04

---
+++ aulas/aula03/src/App.jsx
@@ -0,0 +1,13 @@
+import { Titulo } from "./components/Titulo";
+
+function App() {
+  return (
+  <div>
+    {console.log("Ola console")}
+    <Titulo/>
+    <p>Minha aplicação</p>
+  </div>
+  );
+}
+
+export  default App;
